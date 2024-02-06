13b02d3f3921e49c8819e234da25571209266adb
Aymo <96393445+NotAymo@users.noreply.github.com>
2024-02-06 17:12:50 +0100

    added new version
    pngs are now deprecated

---
+++ signupcont.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sprigatitoverse</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/signup.css">
+</head>
+<body>
+<audio loop autoplay>
+    <source src="assets/audio/bgm.mp3">
+</audio>
+<div class="center">
+	<h1>Before starting,</h1>
+	<h2>Are you a new user or an existing user?</h2>
+	<a href="register.php"><img src="assets/img/newuser_bt.png" class="centernew"></a>
+	<a href="login.php"><img src="assets/img/exuser_bt.png" class="centerold"></a>
+</div>	
+</body>
+</html>
